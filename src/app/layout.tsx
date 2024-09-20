2589d0409aee6d398e842fc3f55f06c8e9bbce83
Ahmed Elsayed <89813459+aalsayed70@users.noreply.github.com>
2024-09-20 14:26:35 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,23 +7,23 @@ const dmSans = Noto_Kufi_Arabic({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "د. ناصر فرحات - إستشاري أول نساء وتوليد والحقن المجهري والمناظير",
-  description: "د. ناصر فرحات، إستشاري أول أمراض النساء والتوليد والحقن المجهري والمناظير، يقدم حلولًا متقدمة في الرعاية الطبية باستخدام تقنيات الذكاء الاصطناعي لتحسين نتائج صحة الأم والجنين.",
+  description: "د. ناصر فرحات، إستشاري أول في أمراض النساء والتوليد والحقن المجهري والمناظير، يقدم حلولًا متقدمة في الرعاية الطبية باستخدام تقنيات الذكاء الاصطناعي لتحسين نتائج صحة الأم والجنين.",
   keywords: "د. ناصر فرحات, إستشاري نساء وتوليد, الحقن المجهري, المناظير, صحة الأم, الحمل, التوليد, الذكاء الاصطناعي, AI solutions in healthcare",
   viewport: "width=device-width, initial-scale=1",
   robots: "index, follow",
-  author: "د. ناصر فرحات",
   openGraph: {
     title: "د. ناصر فرحات - إستشاري أول نساء وتوليد والحقن المجهري والمناظير",
     description: "تعرف على الرعاية الطبية المتطورة مع د. ناصر فرحات، إستشاري أمراض النساء والتوليد.",
     url: "https://www.drnasserfarahat.com",
     type: "website",
+  },
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en" className="relative">
       <body className={clsx(dmSans.className, "antialiased bg-[#ffffff]")}>
