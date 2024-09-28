b97404b0dc0675cfeb30f3d17190c48e4ad39b98
Ahmed Elsayed <89813459+aalsayed70@users.noreply.github.com>
2024-09-28 16:19:54 +0300

    Update Models.tsx

--- src/sections/Models.tsx
+++ src/sections/Models.tsx
@@ -19,21 +19,6 @@ export const Models = () => {
             alt="AI"
             className="w-full h-full object-contain relative z-10"
           />
-        
-
-        <div
-          className="bg-gradient-to-br from-pink-400 to-purple-500 text-center text-white max-w-3xl mx-auto px-6 py-8 rounded-xl shadow-2xl border-2 border-pink-300 relative z-10 mb-6 overflow-hidden"
-        >
-          <div className="absolute inset-0 bg-white opacity-10 transform rotate-3"></div>
-          <p className="text-lg md:text-xl font-medium leading-relaxed relative z-10 text-shadow">
-            النماذج دي طورناها باستخدام بيانات أكثر من
-            <span className="font-bold text-yellow-300"> 3.5 مليون أم</span>،
-            <br className="hidden md:inline" /> تم بناؤها بالكامل من الصفر وهدفها
-            <br className="hidden md:inline" /> تطمني على صحة الحمل وتقديم تحليلات دقيقة
-            <br className="hidden md:inline" /> هتساعدك في حاجات زي
-          </p>
-        </div>
-      </div>
 
       <Modelss />
     </section>
